34cf67132b23558c0ac47e6fa50fc6f947e4f301
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2023-10-29 01:59:26 +0900

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by lvcha mmm on 2023/10/29.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
