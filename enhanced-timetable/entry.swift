22a36b0017abc52eb67229d45ecac942889e8a59
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2023-10-29 17:01:42 +0900

    fix

--- enhanced-timetable/entry.swift
+++ enhanced-timetable/entry.swift
@@ -9,7 +9,28 @@ import WidgetKit
 
 extension enhanced_timetable {
     struct Entry: TimelineEntry {
-        var date: Date = .now
+        var date: Date
+
+        // Default initializer sets date based on current time
+        init() {
+            let now = Date()
+            let hour = Calendar.current.component(.hour, from: now)
+            
+            if hour < 19 {
+                self.date = Calendar.current.date(byAdding: .hour, value: 5, to: now)!
+            } else {
+                if let newDate = Calendar.current.date(byAdding: .hour, value: 3, to: now) {
+                    self.date = newDate
+                } else {
+                    self.date = now  // Fallback in case date addition fails
+                }
+            }
+        }
+        
+        // Additional initializer allows setting a specific date
+        init(date: Date) {
+            self.date = date
+        }
     }
 }
 
--- enhanced-timetable/provider.swift
+++ enhanced-timetable/provider.swift
@@ -9,6 +9,18 @@ import WidgetKit
 
 extension enhanced_timetable {
     struct Provider: TimelineProvider {
+        // Define the class with two Int fields: hour and min
+        class TimePoint {
+            var hour: Int
+            var min: Int
+            var dest: String
+            
+            init(hour: Int, min: Int, dest: String) {
+                self.hour = hour
+                self.min = min
+                self.dest = dest
+            }
+        }
         func placeholder(in context: Context) -> Entry {
             .placeholder
         }
@@ -18,19 +30,66 @@ extension enhanced_timetable {
         }
 
         func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-            let currentDate = Date()
-            let seconds = Calendar.current.component(.second, from: currentDate)
-            // todo, didn't know what to initialize with here
-            var entries = [Entry(date: currentDate)]
-            if let startDate = Calendar.current.date(byAdding: .second, value: seconds, to: currentDate) {
-                entries = (0 ..< 30).map {
-                    if let date = Calendar.current.date(byAdding: .second, value: $0 * 120 - 1, to: startDate) {
-                        return Entry(date: date)
+            let weekdayOjikamiyaTimepoints: [Date] = {
+                var timePoints: [TimePoint] = []
+                
+                timePoints.append(TimePoint(hour: 10, min: 50, dest: "日吉"))
+                
+                // Add some TimePoint instances to the array
+                timePoints.append(TimePoint(hour: 11, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 11, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 11, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 11, min: 38, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 11, min: 50, dest: "日吉"))
+
+                timePoints.append(TimePoint(hour: 12, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 12, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 12, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 12, min: 38, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 12, min: 50, dest: "日吉"))
+
+                timePoints.append(TimePoint(hour: 13, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 13, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 13, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 13, min: 38, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 13, min: 50, dest: "日吉"))
+                
+                timePoints.append(TimePoint(hour: 14, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 14, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 14, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 14, min: 38, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 14, min: 50, dest: "日吉"))
+
+                timePoints.append(TimePoint(hour: 15, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 15, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 15, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 15, min: 38, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 15, min: 50, dest: "日吉"))
+
+                timePoints.append(TimePoint(hour: 16, min: 02, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 16, min: 14, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 16, min: 26, dest: "白金高輪"))
+                timePoints.append(TimePoint(hour: 16, min: 38, dest: "横浜"))
+
+                timePoints.append(TimePoint(hour: 17, min: 0, dest: "日吉"))
+                
+                timePoints.append(TimePoint(hour: 22, min: 39, dest: "日吉"))
+                timePoints.append(TimePoint(hour: 23, min: 20, dest: "横浜"))
+                timePoints.append(TimePoint(hour: 23, min: 58, dest: "白金高輪"))
+                
+                var dates: [Date] = []
+                let calendar = Calendar.current
+                let today = calendar.startOfDay(for: Date())
+                
+                for time in timePoints {
+                    if let date = calendar.date(bySettingHour: time.hour, minute: time.min, second: 0, of: today) {
+                        dates.append(date)
                     }
-                    // todo: didn't know what to return here
-                    return Entry(date: currentDate)
                 }
-            }
+                return dates
+            }()
+            let entries = weekdayOjikamiyaTimepoints.map {return Entry(date: $0)}
+            
             completion(.init(entries: entries, policy: .atEnd))
         }
     }
